78703b42881dac45fd68a5006ab771ee7e92951f
JoeE <JE4440@gmail.com>
2016-06-28 21:38:47 -0400

    added Character class

--- Minions.playground/Contents.swift
+++ Minions.playground/Contents.swift
@@ -2,27 +2,69 @@
 
 import UIKit
 
-class Minion {
+class Character {
     
     typealias Name = String
-    typealias Hearts = Int
     
-    let hearts:Hearts!
+    let name:Name!
     
-    init(hearts: Hearts) { self.hearts = hearts
+    init(name:Name) {
+        self.name = name
         
     }
     
 }
 
-class LadyDeath : Minion {
+class Minion : Character {
     
-    let name:Name! = "Lady Death"
-    let weapon:Weapon! = Weapon(type: .Bow)
+    typealias Hearts = Int
+    typealias Damage = Int
+    
+    let weapon:Weapon!
+    let hearts:Hearts!
+    
+    init(name: Name, hearts: Hearts, weapon:Weapon) {
+        
+        self.hearts = hearts
+        self.weapon = weapon
+        
+        super.init(name: name)
+        
+    }
+    
+    internal func attackMinion(minon:Minion, withWeapon weapon: Weapon) -> Damage {
+        
+        guard let type = weapon.type else { return 0 }
+        switch type {
+        case .Bow:
+            return 5
+        case .Sword:
+            return 5
+            
+        }
+        
+    }
+    
+}
+
+class TheHero : Minion {
+    
+    init() {
+        
+        let hearts:Hearts = 10
+        let name = "Lady Death"
+        let ladyDeathsBow = Weapon(name: "Lady Death's Bow", type: .Bow)
+        super.init(name: name, hearts: hearts, weapon: ladyDeathsBow)
+        
+    }
     
 }
 
 class Weapon {
+    
+    typealias Name = String
+    
+    let name:Name?
     
     enum Action {
         case Swing, Shoot
@@ -34,15 +76,17 @@ class Weapon {
         case Bow, Sword
     }
     
-    let type:Type
+    let type:Type!
     
-    init(type: Type) {
+    init(name:Name, type: Type) {
+        self.name = name
+        
         self.type = type
         
         switch type {
+            
         case .Bow:
             self.action = .Shoot
-            
         case .Sword:
             self.action = .Swing
             
